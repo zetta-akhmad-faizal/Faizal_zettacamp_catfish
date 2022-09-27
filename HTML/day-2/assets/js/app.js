d5acbea5d4e698c94d734fefe5a67b94c4d3d420
akhfzz <akhmad.faizal@zettacamp.pro>
2022-09-27 13:29:33 +0700

    day-three

--- HTML/day-2/assets/js/app.js
+++ HTML/day-2/assets/js/app.js
@@ -1,3 +1,19 @@
 particlesJS.load('particles-js', 'assets/js/particlesjs-config.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+function searchBar(){
+    let table = document.getElementsByTagName("td");
+    let search = document.getElementById("searching");
+    let pagination = document.getElementById("code");
+    let filter = search.value.toUpperCase();
+    for(let i=0;i < table.length; i++){
+        console.log(table[i].outerText.toUpperCase().indexOf(filter))
+        if(table[i].outerText.toUpperCase().indexOf(filter) > -1){
+            table[i].style.display = " ";
+            pagination.innerHTML = `${table[i].cellIndex} of ${table[i].cellIndex}`; 
+        }else{
+            table[i].style.display = "none";
+        }
+    }
+}
