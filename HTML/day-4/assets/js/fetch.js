69cc39fba74ab557b7c5f460edd332604c779d76
akhfzz <akhmad.faizal@zettacamp.pro>
2022-09-29 15:01:14 +0700

    fifth-days

--- HTML/day-4/assets/js/fetch.js
+++ HTML/day-4/assets/js/fetch.js
@@ -1,10 +1,26 @@
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+  }
+  
+function formatDate(date) {
+    return [
+      date.getFullYear(),
+      padTo2Digits(date.getMonth() + 1),
+      padTo2Digits(date.getDate()),
+    ].join('-');
+}
+
 let tbody = document.getElementById("display");
 let data = JSON.parse(localStorage.getItem("arr"));
 let i = 0;
 let init = '';
 let ul = '<ul>';
 for(i; i < data.length; i++){
-    ul += `<li>${data[i].talent[2]}</li>`;
+    let age = data[i].dob.split("-");
+    let dob = Date.now() - new Date(`${age[1]}/${age[2]}/${age[0]}`);
+    var age_dt = new Date(dob);  
+    var year = age_dt.getUTCFullYear(); 
+    var ages = Math.abs(year - 1970); 
     init += `
         <tr>
             <td>${data[i].first_name}</td>
@@ -14,13 +30,13 @@ for(i; i < data.length; i++){
             <td><a href="${data[i].git_acc}">Link</td>
             <td>${data[i].email}</td>
             <td>${data[i].dob}</td>
-            <td>${data[i].mydesc}</td>
+            <td>${data[i].mydesc}. My old is ${ages}</td>
             <td id="telp">${data[i].telp}</td>
             <td>
                 <img src="../assets/img/${data[i].my_image}" alt="myimage" style="widht:50px;height:100px;">
             </td>
         </tr>
-    `
+    `; 
+    console.log(ages)
 }
 tbody.innerHTML = init;
-console.log(ul)
