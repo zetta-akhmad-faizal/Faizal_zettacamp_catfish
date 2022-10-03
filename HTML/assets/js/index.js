f59ccd00c5fa19e2ebd626b0ab3ca592ad2ba959
akhfzz <akhmad.faizal@zettacamp.pro>
2022-10-03 15:03:28 +0700

    revisi-js

--- HTML/assets/js/index.js
+++ HTML/assets/js/index.js
@@ -1,13 +1,15 @@
 function getValue(){
     let dob = document.getElementById('dob').value;
     let old = document.getElementById('old');
+    let old_p = document.getElementById('old-p');
     if(dob === ''){
-        alert('Please completing date field');
+        old_p.innerHTML = "Please fill out your date of birth"
+    }else{
+        let oldSplit = dob.split("/");
+        let getAges = Date.now() - new Date(`${oldSplit[1]}/${oldSplit[2]}/${oldSplit[0]}`);
+        let age_dt = new Date(getAges);
+        let year = age_dt.getUTCFullYear();
+        let ages = Math.abs(year-1970);
+        old.innerHTML = `${ages} years`
     }
-    let oldSplit = dob.split("/");
-    let getAges = Date.now() - new Date(`${oldSplit[1]}/${oldSplit[2]}/${oldSplit[0]}`);
-    let age_dt = new Date(getAges);
-    let year = age_dt.getUTCFullYear();
-    let ages = Math.abs(year-1970);
-    old.innerHTML = `${ages} years`
 }
