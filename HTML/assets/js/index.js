2a2e5e062e5fa994e6995ceb811da959bbee4a60
akhfzz <akhmad.faizal@zettacamp.pro>
2022-10-06 10:33:15 +0700

    save-before-zuhur

--- HTML/assets/js/index.js
+++ HTML/assets/js/index.js
@@ -36,11 +36,70 @@ function callJson(){
                 message.data[i].size.forEach((n) => {
                     init += `${n}/`
                 });
-                size.innerHTML = init
+                size.innerHTML = init;
                 img.src = message.data[i].url;
                 img.alt = `${message.data[i].title} image`;
             }
         }
+    };
+    xmlhttp.send();
+}
+
+function storageAvailableJson(){
+    let order_list = document.getElementById('list-available');
+    var xmlhttp = new XMLHttpRequest(); 
+    var url = "../assets/js/data.json"
+    xmlhttp.open("GET", url, true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.onload = (res) => {
+        let message = JSON.parse(res['target']['response']);
+        let init = ''
+        // console.log(message.data[1].size)
+        for(let i=0; i< message.data.length; i++){
+            init += `
+                <li class="items-storage">
+                    <div class="row">
+                        <div class="column-4">
+                            <img src=${message.data[i].url} alt='${message.data[i].title} image' style="width:100%">
+                        </div>
+                        <div class="column-5">
+                            <h1>${message.data[i].title}</h1>
+                            <p>34 X <span>${message.data[i].price}</span></p>
+                        </div>
+                    </div>
+                </li>
+            `
+        }
+        order_list.innerHTML = init;
+    };
+    xmlhttp.send();
+}
+function storageSoldJson(){
+    let order_list = document.getElementById('list-sold');
+    var xmlhttp = new XMLHttpRequest(); 
+    var url = "../assets/js/data.json"
+    xmlhttp.open("GET", url, true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.onload = (res) => {
+        let message = JSON.parse(res['target']['response']);
+        let init = ''
+        // console.log(message.data[1].size)
+        for(let i=0; i< message.data.length-3; i++){
+            init += `
+                <li class="items-storage">
+                    <div class="row">
+                        <div class="column-4">
+                            <img src=${message.data[i].url} alt='${message.data[i].title} image' style="width:100%">
+                        </div>
+                        <div class="column-5">
+                            <h1>${message.data[i].title}</h1>
+                            <p>34 X <span>${message.data[i].price}</span></p>
+                        </div>
+                    </div>
+                </li>
+            `
+        }
+        order_list.innerHTML = init;
     };
     xmlhttp.send();
 }
