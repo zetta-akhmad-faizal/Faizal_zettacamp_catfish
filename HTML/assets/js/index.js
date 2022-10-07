4f8c9875e6ef013c59a07f54f9397898f53ebae7
akhfzz <akhmad.faizal@zettacamp.pro>
2022-10-07 15:20:45 +0700

    save

--- HTML/assets/js/index.js
+++ HTML/assets/js/index.js
@@ -1,4 +1,4 @@
-function homeData(){
+function homeData() {
     let home = document.getElementById('home');
     let init = '';
     let request = new XMLHttpRequest();
@@ -6,7 +6,7 @@ function homeData(){
     request.setRequestHeader("Content-type", "application/json");
     request.onload = (res) => {
         let message = JSON.parse(res['target']['response']);
-        for(let i=0;i<message.data.length - 6;i++){
+        for (let i = 0; i < message.data.length - 6; i++) {
             init += `
             <article class="items home-basis">
                 <img src='${message.data[i].url}' alt='${message.data[i].title}'>
@@ -22,7 +22,36 @@ function homeData(){
     request.send();
 }
 
-function catalogData(){
+function cardInputByImage(){
+    let price = document.getElementById('price').value;
+    let title = document.getElementById('title').value;
+    let urlInput = document.getElementById('myFile').value;
+    let cardByImage = JSON.parse(localStorage.getItem('cardByImage'));
+    let urlValue = urlInput.split('\\');
+    let url = `../assets/img/${urlValue[2]}`
+
+    let data = {
+        url, title, price
+    }
+    cardByImage.push(data)
+    localStorage.setItem('cardByImage',JSON.stringify(cardByImage))
+}
+
+function cardInputByURL(){
+    let price = document.getElementById('price-url').value;
+    let title = document.getElementById('title-url').value;
+    let url = document.getElementById('myURL').value;
+    let cardByImage = JSON.parse(localStorage.getItem('cardByImage'));
+
+    let data = {
+        url, title, price
+    }
+    console.log(url)
+    cardByImage.push(data)
+    localStorage.setItem('cardByImage',JSON.stringify(cardByImage))
+}
+
+function catalogData() {
     let home = document.getElementById('catalog');
     let init = '';
     let request = new XMLHttpRequest();
@@ -30,7 +59,7 @@ function catalogData(){
     request.setRequestHeader("Content-type", "application/json");
     request.onload = (res) => {
         let message = JSON.parse(res['target']['response']);
-        for(let i=0;i<message.data.length;i++){
+        for (let i = 0; i < message.data.length; i++) {
             init += `
             <article class="items shopping-basis">
                 <img src='${message.data[i].url}' alt='${message.data[i].title}'>
@@ -42,4 +71,123 @@ function catalogData(){
         home.innerHTML = init
     };
     request.send();
+}
+
+function cardData() {
+    let home = document.getElementById('card');
+    let init = '';
+    let request = localStorage.getItem('cardByImage');
+    let message = JSON.parse(request);
+    for (let i = 0; i < message.length; i++) {
+        init += `
+        <article class="items shopping-basis">
+            <img src='${message[i].url}' alt='${message[i].title}'>
+            <p>${message[i].title}</p>
+            <span>${message[i].price}</span>
+        </article>
+        `;
+    }
+    home.innerHTML = init
+}
+function cardData() {
+    let home = document.getElementById('card');
+    let init = '';
+    let request = localStorage.getItem('cardByImage');
+    let message = JSON.parse(request);
+    for (let i = 0; i < message.length; i++) {
+        init += `
+        <article class="items shopping-basis">
+            <img src='${message[i].url}' alt='${message[i].title}'>
+            <p>${message[i].title}</p>
+            <span>${message[i].price}</span>
+        </article>
+        `;
+    }
+    home.innerHTML = init
+}
+
+function openForm(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "flex";
+    evt.currentTarget.className += " active";
+}
+
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+
+    dropZoneElement.addEventListener("click", (e) => {
+        inputElement.click();
+    });
+
+    inputElement.addEventListener("change", (e) => {
+        if (inputElement.files.length) {
+            updateThumbnail(dropZoneElement, inputElement.files[0]);
+        }
+    });
+
+    dropZoneElement.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        dropZoneElement.classList.add("drop-zone--over");
+    });
+
+    ["dragleave", "dragend"].forEach((type) => {
+        dropZoneElement.addEventListener(type, (e) => {
+            dropZoneElement.classList.remove("drop-zone--over");
+        });
+    });
+
+    dropZoneElement.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        if (e.dataTransfer.files.length) {
+            inputElement.files = e.dataTransfer.files;
+            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+        }
+
+        dropZoneElement.classList.remove("drop-zone--over");
+    });
+});
+
+/**
+ * Updates the thumbnail on a drop zone element.
+ *
+ * @param {HTMLElement} dropZoneElement
+ * @param {File} file
+ */
+function updateThumbnail(dropZoneElement, file) {
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+
+    // First time - remove the prompt
+    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+        dropZoneElement.querySelector(".drop-zone__prompt").remove();
+    }
+
+    // First time - there is no thumbnail element, so lets create it
+    if (!thumbnailElement) {
+        thumbnailElement = document.createElement("div");
+        thumbnailElement.classList.add("drop-zone__thumb");
+        dropZoneElement.appendChild(thumbnailElement);
+    }
+
+    thumbnailElement.dataset.label = file.name;
+
+    // Show thumbnail for image files
+    if (file.type.startsWith("image/")) {
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+        };
+    } else {
+        thumbnailElement.style.backgroundImage = null;
+    }
 }
