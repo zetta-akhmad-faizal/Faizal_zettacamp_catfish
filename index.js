2eb8101d8f0db346fb63846e2a8b5fc9af6734e9
akhfzz <akhmad.faizal@zettacamp.pro>
2022-10-11 11:28:18 +0700

    js-day1

---
+++ index.js
@@ -0,0 +1,41 @@
+let request = require('request');
+
+const options = {
+    url: 'https://laravel-books-db.herokuapp.com/api/books?page=1&category=historical-fiction&language=en',
+    headers: {
+        'User-Agent': 'request',
+        'Authorization': 'Bearer 175|35rv9k0sTZLGyOSpUd2Db171SkqAqCo8QMiKyWO3'
+    }
+};
+
+let purchasingBook = (detail, price) => {
+    let discount = 30/100;
+    let tax = 10/100;
+    let getPrice = price.split(" ");
+
+    let amountOfDiscount = parseFloat(getPrice[1]) * discount;
+    let priceAfterDiscount = parseFloat(getPrice[1]) - amountOfDiscount;
+    let amountOfTax = priceAfterDiscount * tax;
+    let amountAfterTax = priceAfterDiscount + amountOfTax 
+    console.log(`
+        Detail of Book: ${detail}
+        Percentage of Discount: ${discount}, so amount of discount is Rp. ${amountOfDiscount}00
+        Percentage of Tax: ${tax}, so amount of tax is Rp. ${amountOfTax}0
+        The books must be paid Rp. ${amountAfterTax}0
+    `)
+}
+
+let getBooks = async(error, response, body) =>  {
+    if (!error && response.statusCode == 200) {
+        const info = await JSON.parse(body);
+        info.books.map(b => {
+            purchasingBook(b.title, b.price)
+        })
+    }
+}
+
+try{
+    request(options, getBooks)
+}catch(e){
+    console.log(e)
+};
